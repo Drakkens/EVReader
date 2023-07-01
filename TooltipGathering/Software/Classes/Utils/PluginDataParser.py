b9d801a5cc093c2cfc397e602aa0ecc0a232dbe8
Drakkens <72207118+Drakkens@users.noreply.github.com>
2023-07-01 23:30:24 +0200

    Auto PluginData file finder

--- TooltipGathering/Software/Classes/Utils/PluginDataParser.py
+++ TooltipGathering/Software/Classes/Utils/PluginDataParser.py
@@ -3,7 +3,17 @@ import re
 
 LOTRO_DIR = os.path.expanduser(r"~\Documents\The Lord of the Rings Online\PluginData")
 
-current_profile_file = open(LOTRO_DIR + r"\maxusdrago\Crickhollow\Drakensbear\EVCTest.plugindata")
+possible_files = {}
+
+for root, dirs, files in os.walk(LOTRO_DIR):
+    for file in files:
+        if "EVC" in file:
+            possible_files[os.path.getmtime(root + "\\" + file)] = root + "\\" + file
+
+sorted_files = dict(sorted(possible_files.items(), reverse=True))
+last_modified_file = next((iter(sorted_files.items())))[1]
+
+current_profile_file = open(last_modified_file)
 
 content = current_profile_file.read()
 
@@ -15,7 +25,7 @@ essence_weights = {}
 for match in pattern.finditer(content):
     stat_name = match.group(1)
     stat_weight = match.group(2)
-    
+
     essence_weights[stat_name] = stat_weight
-    
+
 print(essence_weights)
