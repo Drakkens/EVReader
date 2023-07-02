a3e069dab782e61fd026e29d8454d9e65b716646
Drakkens <72207118+Drakkens@users.noreply.github.com>
2023-07-02 23:54:21 +0200

    Cleaned up Console Outputs

--- TooltipGathering/Plugin/EVC/Child.lua
+++ TooltipGathering/Plugin/EVC/Child.lua
@@ -5,7 +5,11 @@ import "Turbine.Gameplay";
 
 Turbine.Shell.WriteLine('DataLoader!')
 
-local essenceList = { 'Critical Rating',
+local loaded = false;
+
+local essenceList = { 'Maximum Morale',
+                      'Vitality',
+                      'Critical Rating',
                       'Finesse Rating',
                       'Physical Mastery Rating',
                       'Tactical Mastery Rating',
@@ -19,14 +23,44 @@ local essenceList = { 'Critical Rating',
                       'Incoming Healing Rating',
                       'Resistance Rating' };
 
+function readData()
+    relevantData = {};
+
+    local cmds = Turbine.Shell.GetCommands();
+
+    for command_id, EVCData in pairs(cmds) do
+        if (string.find(EVCData, "EVC")) then
+            EVCData = string.gsub(EVCData, 'EVC', '')
+
+            local hyphenIndex = string.find(EVCData, '-');
+
+            local EVCStatName = string.sub(EVCData, 0, hyphenIndex - 1)
+            local EVCStatValue = string.sub(EVCData, hyphenIndex + 1, -1)
+
+            relevantData[EVCStatName] = EVCStatValue;
+        end
+    end
+end
+
+readData();
+
 function UnloadPlugin()
-    Turbine.PluginData.Save(Turbine.DataScope.Character, "EVCTest", statWeights, eventHandler);
-    Turbine.Shell.WriteLine('Data Loader Plugin Unloading...');
+    function eventHandler()
+        Turbine.Shell.WriteLine('Data Saved');
+
+    end
+
+    --Turbine.PluginData.Save(Turbine.DataScope.Character, "EVCTest", relevantData, eventHandler);
+    Turbine.PluginData.Save(Turbine.DataScope.Character, "EVCData", relevantData, eventHandler);
 
 
 end
 
-Plugins['EVCDataLoader'].Unload = function()
-    UnloadPlugin();
 
+if (Plugins['EVCDataLoader'] ~= nil and not loaded) then
+    loaded = true;
+    Plugins['EVCDataLoader'].Unload = function()
+        UnloadPlugin();
+
+    end
 end
